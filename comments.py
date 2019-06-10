a98677605ffbb1f6d696860666502cad8e66a0da
Kerem Kandirali <keremkandirali@gmail.com>
2019-06-10 19:16:15 +0300

    Added

--- comments.py
+++ comments.py
@@ -1,20 +1,17 @@
 import random
 
-author_name = 'WPSelected'
-author_email = 'wpselected@gmail.com'
-author_website = 'https://www.wpselected.com'
+author_name = 'YourName'
+author_email = 'YourEmail@gmail.com'
+author_website = 'https://www.YourSite.com'
 
 comments = [
-    'Your article touches some important points. It was really helpful for my research. Thanks.',
-    'Explanatory, compact and reader-friendly. You made a good job with this post, thanks for sharing!',
-    'Generally speaking, I think there is a big need for this kind of useful articles. Thank you!',
-    'Great timing :) I was just looking for additional information about this.',
-    'I wish to see more blog posts in this clear and informative style.',
-    'This is a "must read" article! Already read more than once. Thanks a lot.',
-    'It\'s enlightening for the ones who wonder about this topic. Congrats!',
+    'Your comment 1',
+    'Your comment 2',
+    'Your comment 3',
+    'Your comment 4',
+    'Your comment 5',
 ]
 
 
 def random_comment():
     return comments[random.randrange(len(comments))]
-
--- sites.py
+++ sites.py
@@ -1,20 +1,20 @@
 sites = [
     {
-        'url': 'https://wpmayor.com/',
-        'link_selector': 'h2.post-title a',
-        'title_selector': 'h1.post-title',
-        'author_selector': 'cite.fn a',
-        'submit_selector': 'submit',
+        'url': 'https://www.site1.com/',  # URL of the blog posts.
+        'link_selector': 'h2.post-title a',  # Link selector of the first post in the loop. e.g. <h2 class="post-title"><a href="#">The Post</a></h2>
+        'title_selector': 'h1.post-title',  # Title selector of the selected post. e.g. <h1 class="post-title">The Post</h1>
+        'author_selector': 'cite.fn a',  # Author name selector of the comments section. e.g. <cite class="fn"><a href="#">The Author</a></cite>
+        'submit_selector': 'submit',  # Submit button selector (id) of the comments section. e.g. <input type="submit" id="submit">Submit</input>
     },
     {
-        'url': 'https://wptavern.com/',
+        'url': 'https://www.site2.com/',
         'link_selector': 'h2.entry-title a',
         'title_selector': 'h1.entry-title',
         'author_selector': 'cite.comment-author a span',
         'submit_selector': 'submit',
     },
     {
-        'url': 'https://wpshout.com/articles/',
+        'url': 'https://www.site3.com/',
         'link_selector': '.archive-article a',
         'title_selector': 'h1.article-title',
         'author_selector': 'cite.fn a',
